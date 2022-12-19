eecc295207d1d1e8769ccf822189a5b6b2f134cf
Qiaolin Yu <liin1211@outlook.com>
2022-12-19 16:49:15 +0800

    refactor test

--- autoscale/aws_sns_test.go
+++ autoscale/aws_sns_test.go
@@ -2,16 +2,14 @@ package autoscale
 
 import "testing"
 
-func TestCreateTopic(t *testing.T) {
+func TestAwsSns(t *testing.T) {
 	err := CreateTopic("autoscale_test")
 	if err != nil {
 		t.Errorf("[error]Create topic failed, err: %+v\n", err.Error())
 		return
 	}
-}
 
-func TestPublishTopology(t *testing.T) {
-	err := PublishTopology("autoscale_test", "12:01", []string{"aaa", "bbb"})
+	err = PublishTopology("autoscale_test", "12:01", []string{"aaa", "bbb"})
 	if err != nil {
 		t.Errorf("[error]Publish message failed, err: %+v\n", err.Error())
 		return
