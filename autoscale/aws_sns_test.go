f85c56a3557c056c5df23e3830cab54013539d55
Qiaolin Yu <liin1211@outlook.com>
2022-12-20 16:45:56 +0800

    refactor test

--- autoscale/aws_sns_test.go
+++ autoscale/aws_sns_test.go
@@ -21,19 +21,21 @@ func TestAwsSns(t *testing.T) {
 	}
 }
 
+func test(awsSnsManager *AwsSnsManager, i int) {
+	log.Printf("start func: %d", i)
+	now := time.Now()
+	ts := now.UnixNano()
+	err := awsSnsManager.TryToPublishTopology(string(rune(i)), ts, []string{"a"})
+	if err != nil {
+		log.Printf("[error]Create topic failed, err: %+v\n", err.Error())
+		return
+	}
+}
+
 func TestCurrent(t *testing.T) {
 	awsSnsManager := NewAwsSnsManager("us-east-2")
 	for i := 0; i < 50; i++ {
-		go func(i int) {
-			log.Printf("start func: %dn", i)
-			now := time.Now()
-			ts := now.UnixNano()
-			err := awsSnsManager.TryToPublishTopology(string(rune(i)), ts, []string{"a"})
-			if err != nil {
-				t.Errorf("[error]Create topic failed, err: %+v\n", err.Error())
-				return
-			}
-		}(i)
+		go test(awsSnsManager, i)
 	}
 
 }
