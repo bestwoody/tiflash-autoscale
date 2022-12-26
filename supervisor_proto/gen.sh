8ca86b1d45a9035e2362a9f34e473af50133a664
bestwoody <bestwoody@163.com>
2022-12-26 21:28:58 +0800

    protoc-gen-go@v1.3.0

--- supervisor_proto/gen.sh
+++ supervisor_proto/gen.sh
@@ -1,1 +1,6 @@
+# !!! PLEASE use protoc-gen-go@v1.3.0
+# go install github.com/golang/protobuf/protoc-gen-go@v1.3.0
 protoc     --go_out=plugins=grpc:.    supervisor.proto 
+#protoc --go_out=.  \
+#    --go-grpc_out=.  \
+#    supervisor.proto
