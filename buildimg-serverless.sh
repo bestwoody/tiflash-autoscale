4dccbfe8e72d431323a61c001b4e97d325181831
bestwoody <bestwoody@163.com>
2023-02-07 17:18:33 +0800

    update buildimg script

--- buildimg-serverless.sh
+++ buildimg-serverless.sh
@@ -1,3 +1,8 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'need args!'
+    exit 1
+fi
 #docker build -t bestwoody/tiflash-autoscaler:serverless.v6.4.0 . && docker push bestwoody/tiflash-autoscaler:serverless.v6.4.0
 # docker buildx build --platform=linux/amd64,linux/arm64 -t bestwoody/tiflash-autoscaler:serverless.v6.4.0 .  --push
-docker buildx build --platform=linux/amd64,linux/arm64 -t 646528577659.dkr.ecr.us-east-2.amazonaws.com/tiflash-autoscaler:serverless.v6.4.0 .  --push
+# docker buildx build --platform=linux/amd64,linux/arm64 -t 646528577659.dkr.ecr.us-east-2.amazonaws.com/tiflash-autoscaler:serverless.v6.4.0 .  --push
+docker buildx build --platform=linux/amd64,linux/arm64 -t 646528577659.dkr.ecr.us-east-2.amazonaws.com/tiflash-autoscaler:$1 .  --push
--- buildimg.sh
+++ buildimg.sh
@@ -1,5 +1,5 @@
 if [[ $# -eq 0 ]] ; then
-    echo 'some message'
+    echo 'need args!'
     exit 1
 fi
 docker build -t bestwoody/k8stest:$1 . && docker push bestwoody/k8stest:$1
