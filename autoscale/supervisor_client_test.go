96935f5bff23828e0bfad3ce9335fe3069e1bfff
Qiaolin Yu <liin1211@outlook.com>
2023-03-02 14:06:20 +0800

    mock grpc server

--- autoscale/supervisor_client_test.go
+++ autoscale/supervisor_client_test.go
@@ -1,10 +1,40 @@
 package autoscale
 
 import (
+	context "context"
+	pb "github.com/tikv/pd/supervisor_proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"sync/atomic"
 	"testing"
 )
 
-func TestAssignAndUnassignTenant(t *testing.T) {
+var (
+	podIP      = "127.0.0.1"
+	tenantName = "test-tenant"
 
-	AssignTenantHardCodeArgs("1", "1")
+	// mock variables in server
+	tenantNameInServer atomic.Value
+	isUnassigning      atomic.Bool
+)
+
+func (s *server) AssignTenant(ctx context.Context, in *pb.AssignRequest) (*pb.Result, error) {
+	tenantNameInServer.Store(in.GetTenantID())
+	return &pb.Result{HasErr: false, ErrInfo: "", TenantID: tenantNameInServer.Load().(string), IsUnassigning: isUnassigning.Load()}, nil
+}
+
+func (s *server) UnassignTenant(ctx context.Context, in *pb.UnassignRequest) (*pb.Result, error) {
+	tenantNameInServer.Store("")
+	if in.AssertTenantID != tenantNameInServer.Load() {
+		return &pb.Result{HasErr: true, ErrInfo: "assert tenant id failed", TenantID: tenantNameInServer.Load().(string), IsUnassigning: isUnassigning.Load()}, nil
+	}
+	return &pb.Result{HasErr: false, ErrInfo: "", TenantID: tenantNameInServer.Load().(string), IsUnassigning: isUnassigning.Load()}, nil
+}
+
+func (s *server) GetCurrentTenant(ctx context.Context, empty *emptypb.Empty) (*pb.GetTenantResponse, error) {
+	return &pb.GetTenantResponse{TenantID: tenantNameInServer.Load().(string), IsUnassigning: isUnassigning.Load()}, nil
+}
+
+func TestAssignAndUnassignTenant(t *testing.T) {
+	AssignTenantHardCodeArgs(podIP, tenantName)
+	UnassignTenant(podIP, tenantName, true)
 }
